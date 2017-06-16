f9c8de471a9277e16e3e742675fbfdd289f606a6
Phillip Beck <phillipcbeck@gmail.com>
2017-06-16 09:08:00 -0300

    Alterado aruqivo de configuracao

--- src/config/production/index.js
+++ src/config/production/index.js
@@ -4,6 +4,5 @@ module.exports = {
   fixedLayout: false,
   hideLogoOnMobile: false,
   clientId: 'fae5bde3-ea4b-417a-aeb1-099825bd91cc',
-  clientSecret: 'ad472ecd-0646-403a-bd32-5362e734d2b6',
-  redirectUri: 'http://localhost:8082/token'
+  clientSecret: 'ad472ecd-0646-403a-bd32-5362e734d2b6'
 }
