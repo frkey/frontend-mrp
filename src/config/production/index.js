c6ab7ac89233d36cca079f6aa8c1c401944d038e
Phillip Beck <phillipcbeck@gmail.com>
2017-06-26 15:14:38 -0300

    ajustadas configuracoes para production

--- src/config/production/index.js
+++ src/config/production/index.js
@@ -1,8 +1,8 @@
 module.exports = {
-  frontendAddress: 'http://192.168.0.37:8082',
-  apiGatewayUri: 'http://192.168.0.37:3000',
+  frontendAddress: '',
+  apiGatewayUri: '',
   fixedLayout: false,
   hideLogoOnMobile: false,
-  clientId: 'fae5bde3-ea4b-417a-aeb1-099825bd91cc',
-  clientSecret: 'ad472ecd-0646-403a-bd32-5362e734d2b6'
+  clientId: '',
+  clientSecret: ''
 }
