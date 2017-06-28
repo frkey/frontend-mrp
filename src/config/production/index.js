a57d71f4ca32c73eebcab8ffc22b276e2b0fa3b1
Phillip Beck <phillipcbeck@gmail.com>
2017-06-28 10:19:06 -0300

    Endereços AWS

--- src/config/production/index.js
+++ src/config/production/index.js
@@ -1,8 +1,8 @@
 module.exports = {
-  frontendAddress: '',
-  apiGatewayUri: '',
+  frontendAddress: 'https://umaflex.pgbr.net',
+  apiGatewayUri: 'https://api-umaflex.pgbr.net',
   fixedLayout: false,
   hideLogoOnMobile: false,
-  clientId: '',
-  clientSecret: ''
+  clientId: '42b49dcc-29ad-4694-bd54-dd1c6e1f44ab',
+  clientSecret: '1868dfdd-371a-439e-a66c-cfe9867404a4'
 }
