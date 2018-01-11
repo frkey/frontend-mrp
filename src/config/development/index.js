ba5a49fea108ea4dfdd4cba49f83f9762612513f
Phillip Beck <phillipcbeck@gmail.com>
2018-01-10 23:34:13 -0200

    Added development config file

---
+++ src/config/development/index.js
@@ -0,0 +1,8 @@
+module.exports = {
+    frontendAddress: 'https://umaflex.pgbr.net',
+    apiGatewayUri: 'https://api-umaflex.pgbr.net',
+    fixedLayout: false,
+    hideLogoOnMobile: false,
+    clientId: '1c06578f-a49f-4476-97f4-8860a71bee31',
+    clientSecret: '180accaa-4143-4479-8218-d752ff837b26'
+  }
